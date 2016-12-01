43a761cc85cdc1fc67cdd5cb3f61bec9f8e859b9
João Fernandes <joaovitorfernandes2@gmail.com>
2016-12-01 03:20:00 -0200

    Complete lesson 101

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 	root 'welcome#index'
 	get 'about', to: 'welcome#about'
 
+	get 'singup', to: 'users#new'
+	resources :users, except: [:new]
+
 	resources :articles
 end
