8e546b9bb8faa6d722ba7fa4634676accce62f05
João Fernandes <joaovitorfernandes2@gmail.com>
2016-12-05 12:20:30 -0200

    Complete lesson 152

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
 
 	before_action :require_admin, except: [:index, :show]
+	before_action :set_category, only: [:edit, :update, :show, :destroy]
 
 	def index
 		@categories = Category.paginate(page: params[:page], per_page: 5)
@@ -20,12 +21,30 @@ class CategoriesController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @category.update(category_params)
+			flash[:success] = "Category updated!"
+			redirect_to category_path(@category)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+	end
+
 	def show
-		@category = Category.find(params[:id])
 		@category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
 	end
 
 	private
+	def set_category
+		@category = Category.find(params[:id])
+	end
+
 	def category_params
 		params.require(:category).permit(:name)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
 	delete 'logout', to: 'sessions#destroy'
 
 	resources :articles
-	resources :categories, except: [:destroy]
+	resources :categories
 end
